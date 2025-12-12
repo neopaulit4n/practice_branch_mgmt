fc43664e5c9db3de6943cc41f2bf1cdc9d905d3f
ashtondrew <ashton.drew@gmail.com>
2025-12-11 20:33:29 -0500

    prep: data imported

---
+++ airquality_analysis.R
@@ -0,0 +1,9 @@
+# Creating a repo for practicing branch management
+# December 2025
+
+# Packages ----
+library(dplyr)
+library(lubridate)
+
+# Import data ----
+air <- read.csv("airquality_raw.csv")
