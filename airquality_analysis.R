064e74a23e70bcafff6ba03bf4801b165af2874c
ashtondrew <ashton.drew@gmail.com>
2025-12-11 20:37:10 -0500

    Prep: temp to celcius

--- airquality_analysis.R
+++ airquality_analysis.R
@@ -7,3 +7,9 @@ library(lubridate)
 
 # Import data ----
 air <- read.csv("airquality_raw.csv")
+
+# Tidy the Data ----
+
+# Convert temperature to celcius
+air$Temp <- round(5/9 * (air$Temp - 32), 1)
+
